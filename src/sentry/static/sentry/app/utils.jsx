9daeb55ea0ed0b772853e1a1313b5d9216767c97
Billy Vong <billy@sentry.io>
2018-11-05 12:52:31 -0500

    add tests, fix state bug with timerangeselector and absolute item always being selected, when changing to absolute dates, now use equiv of previously selected relative range

--- src/sentry/static/sentry/app/utils.jsx
+++ src/sentry/static/sentry/app/utils.jsx
@@ -236,6 +236,27 @@ export function descopeFeatureName(feature) {
     : feature.match(/(?:^(?:projects|organizations):)?(.*)/).pop();
 }
 
+/**
+ * This parses our period shorthand strings (e.g. <int><unit>)
+ * and converts it into hours
+ */
+export function parsePeriodToHours(str) {
+  const [, periodNumber, periodLength] = str.match(/([0-9]+)([mhdw])/);
+
+  switch (periodLength) {
+    case 'm':
+      return periodNumber / 60;
+    case 'h':
+      return periodNumber;
+    case 'd':
+      return periodNumber * 24;
+    case 'w':
+      return periodLength * 24 * 7;
+    default:
+      return -1;
+  }
+}
+
 // re-export under utils
 export {parseLinkHeader, Collection, PendingChangeQueue, CursorPoller};
 
