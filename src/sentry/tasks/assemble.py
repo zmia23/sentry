4e65ae17fd92b242a11d47917effeb4606b901db
Joshua Li <josh@jrl.ninja>
2019-06-24 14:57:02 -0700

    missed a few spots
    (cherry picked from commit 6aeee2ab30a22d33e0dd1ec49f7e6bed0ae8ca3c)

--- src/sentry/tasks/assemble.py
+++ src/sentry/tasks/assemble.py
@@ -262,7 +262,7 @@ def assemble_artifacts(org_id, version, checksum, chunks, **kwargs):
 
     except AssembleArtifactsError as e:
         set_assemble_status(AssembleTask.ARTIFACTS, org_id, checksum,
-                            ChunkFileState.ERROR, detail=e.message)
+                            ChunkFileState.ERROR, detail=str(e))
     except BaseException:
         logger.error('failed to assemble release bundle', exc_info=True)
         set_assemble_status(AssembleTask.ARTIFACTS, org_id, checksum,
