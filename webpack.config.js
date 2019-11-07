a6d1050840a97d5ea3b6b2f432bf811a7785b439
Stephen Cefali <scefali@sentry.io>
2019-11-06 16:26:26 -0800

    adds plugin webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -457,4 +457,71 @@ if (IS_PRODUCTION) {
   });
 }
 
-module.exports = appConfig;
+const PLUGIN_APPS = ['jira', 'sessionstack'];
+
+function getPluginConfig(app) {
+  const pyName = app.replace('-', '_');
+  const staticPluginPrefix = `src/new_sentry_plugins/${pyName}/static/${pyName}`;
+  const distPluginPath = `${staticPluginPrefix}/dist`;
+
+  const config = {
+    mode: WEBPACK_MODE,
+    name: app,
+    entry: `./${pyName}.jsx`,
+    context: path.join(__dirname, staticPluginPrefix),
+    externals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'react-router': 'Router',
+      reflux: 'Reflux',
+      moment: 'moment',
+      sentry: 'SentryApp',
+      'prop-types': 'PropTypes',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: path.join(__dirname, staticPluginPrefix),
+          exclude: /(vendor|node_modules)/,
+          use: [
+            {
+              loader: 'babel-loader',
+              // Disable loading the configFile for when plugins are being
+              // built by a webpack devserver *outside* of this directory.
+              // Otherwise the loader will try and locate the babel.config.js
+              // file, which will be sentry or getsentrys.
+              options: {...babelConfig, configFile: false},
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [new LodashModuleReplacementPlugin()],
+    resolve: {
+      extensions: ['*', '.jsx', '.js'],
+    },
+    output: {
+      path: path.join(__dirname, distPluginPath),
+      filename: `${app}.js`,
+      sourceMapFilename: `${app}.js.map`,
+    },
+    devtool: IS_PRODUCTION ? 'source-map' : 'cheap-module-eval-source-map',
+  };
+
+  // This compression-webpack-plugin generates pre-compressed files
+  // ending in .gz, to be picked up and served by our internal static media
+  // server as well as nginx when paired with the gzip_static module.
+  if (IS_PRODUCTION) {
+    config.plugins.push(
+      new CompressionPlugin({
+        algorithm: 'gzip',
+        test: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/,
+      })
+    );
+  }
+
+  return config;
+}
+
+module.exports = [appConfig, ...PLUGIN_APPS.map(getPluginConfig)];
