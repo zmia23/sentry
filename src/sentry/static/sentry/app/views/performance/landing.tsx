e86a0e90bf14570898febadc3e600893fda353f3
Alberto Leal <mail4alberto@gmail.com>
2020-02-08 00:31:08 -0500

    feat(perf-view): Initial landing page

---
+++ src/sentry/static/sentry/app/views/performance/landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type Props = {};
+
+class PerformanceLanding extends React.Component<Props> {
+  render() {
+    return <div>foo</div>;
+  }
+}
+
+export default PerformanceLanding;
